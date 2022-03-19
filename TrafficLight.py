f6bcdb343688a79e8174d1abbff1b2209ff658b9
SharmaManjul <manjul199912@gmail.com>
2022-03-18 20:47:25 -0500

    Added setters for class Traffic Light.

--- TrafficLight.py
+++ TrafficLight.py
@@ -9,8 +9,21 @@ class TrafficLight:
     def get_traffic(self):
         return self.traffic
 
+    def set_light_state(self, light_state):
+        self.light_state = light_state
 
-l_1 = TrafficLight(1, 23)
+    def set_traffic(self, traffic):
+        self.traffic = traffic
+
+
+l_1 = TrafficLight(0,0)
+
+print(l_1.get_light_state())
+print(l_1.get_traffic())
+
+l_1.set_light_state(2)
+l_1.set_traffic(13)
+
 print(l_1.get_light_state())
 print(l_1.get_traffic())
 
