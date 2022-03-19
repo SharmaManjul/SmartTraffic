41109eec4c86a79a3ee6f4dfd04012e55b2a7848
SharmaManjul <manjul199912@gmail.com>
2022-03-18 20:43:21 -0500

    Added getters for class TrafficLight.

--- TrafficLight.py
+++ TrafficLight.py
@@ -2,3 +2,15 @@ class TrafficLight:
     def __init__(self, light_state, traffic):
         self.light_state = light_state
         self.traffic = traffic
+
+    def get_light_state(self):
+        return self.light_state
+
+    def get_traffic(self):
+        return self.traffic
+
+
+l_1 = TrafficLight(1, 23)
+print(l_1.get_light_state())
+print(l_1.get_traffic())
+
