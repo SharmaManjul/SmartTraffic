cf33ee5228c61503fea8c0ce40bc6cc133449ba4
SharmaManjul <manjul199912@gmail.com>
2022-03-20 13:41:34 -0500

    Refactored class Traffic Light to make it more DRY.

--- TrafficLight.py
+++ TrafficLight.py
@@ -7,15 +7,15 @@ class TrafficLight:
         self.g_time = g_time
 
     def traffic_flow(self):
+        print("Light state inside traffic flow: ", self.light_state)
         if self.light_state == 1:
             self.traffic += self.r_time * 5  # 5 cars per minute getting added
         elif self.light_state == 2:
-            self.traffic += self.y_time * 3
+            self.traffic += self.y_time * 2
         elif self.light_state == 3:
-            self.traffic -= self.g_time * 10
+            self.traffic -= self.g_time * 4
         else:
-            return "Invalid Light State."
-        return self.traffic
+            print("Invalid Light State.")
 
     def get_light_state(self):
         return self.light_state
@@ -23,55 +23,39 @@ class TrafficLight:
     def get_traffic(self):
         return self.traffic
 
-    def get_r_time(self):
+    def get_traffic_time(self):
         return self.r_time
-
-    def get_y_time(self):
         return self.y_time
-
-    def get_g_time(self):
         return self.g_time
 
     def set_light_state(self, light_state):
         self.light_state = light_state
+        self.traffic_flow()
 
     def set_traffic(self, traffic):
         self.traffic = traffic
 
-    def set_r_time(self, r_time):
+    def set_traffic_time(self, r_time, y_time, g_time):
         self.r_time = r_time
-
-    def set_y_time(self, y_time):
         self.y_time = y_time
-
-    def set_g_time(self, g_time):
         self.g_time = g_time
 
 
 l1 = TrafficLight(0, 0, 0, 0, 0)
+l1.set_traffic_time(3, 0.05, 3)
+l1.set_traffic(0)
 
 print(l1.get_light_state())
 print(l1.get_traffic())
 
 l1.set_light_state(1)
-l1.set_traffic(13)
-l1.set_r_time(3)
-l1.set_y_time(0.1)
-l1.set_g_time(2)
-
-print(l1.get_light_state())
-print(l1.get_traffic())
-
-l1.traffic_flow()
 print(l1.get_light_state())
 print(l1.get_traffic())
 
 l1.set_light_state(2)
-l1.traffic_flow()
 print(l1.get_light_state())
 print(l1.get_traffic())
 
 l1.set_light_state(3)
-l1.traffic_flow()
 print(l1.get_light_state())
 print(l1.get_traffic())
